4993f91dd750b91f72861e2b83311cecdc239a33
FanjeauxGael <44019467+FanjeauxGael@users.noreply.github.com>
2022-01-15 11:35:16 +0100

    first commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        //récupere PlayerMotor 
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        // Calculer la vitesse du mouvement de notre joueur
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * xMov;
+        Vector3 moveVertical = transform.forward * zMov;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+    }
+
+}
