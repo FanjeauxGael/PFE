7d75c1d1c9c690b395bcdd24210dad5091b073f6
damienbrt <damien09200@gmail.com>
2022-03-13 11:31:52 +0100

    Tuto 12 et 13 weapon manager et particule de tir

---
+++ Assets/WeaponManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class WeaponManager : NetworkBehaviour
+{
+    [SerializeField]
+    private PlayerWeapon primaryWeapon;
+
+    private PlayerWeapon currentWeapon;
+    private WeaponGraphics currentGraphics;
+
+    [SerializeField]
+    private Transform weaponHolder;
+
+    [SerializeField]
+    private string weaponLayerName = "Weapon";
+
+    void Start()
+    {
+        EquipWeapon(primaryWeapon);
+    }
+
+    public PlayerWeapon GetCurrentWeapon()
+    {
+        return currentWeapon;
+    }
+
+    public WeaponGraphics GetCurrentGraphics()
+    {
+        return currentGraphics;
+    }
+
+    void EquipWeapon(PlayerWeapon _weapon)
+    {
+        currentWeapon = _weapon;
+
+        GameObject weaponIns = Instantiate(_weapon.graphics, weaponHolder.position, weaponHolder.rotation);
+        weaponIns.transform.SetParent(weaponHolder);
+
+        currentGraphics = weaponIns.GetComponent<WeaponGraphics>();
+
+        if(currentGraphics == null)
+        {
+            Debug.LogError("Pas de script WeaponGraphics sur l'arme : " + weaponIns.name);
+        }
+
+        if (isLocalPlayer)
+        {
+            Util.SetLayerRecursively(weaponIns, LayerMask.NameToLayer(weaponLayerName));
+        }
+    }
+}
