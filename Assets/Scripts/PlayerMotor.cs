348f26b05dc2496883838946eec0d082e3fef939
damienbrt <damien09200@gmail.com>
2022-05-07 12:09:00 +0200

    Character Jump

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -12,10 +12,29 @@ public class PlayerMotor : MonoBehaviour
 
     private Rigidbody rb;
 
+    public LayerMask groundLayers;
+    public float jumpForce = 7;
+    public SphereCollider col;
+    
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        col = GetComponent<SphereCollider>();
     }
+
+    public void Update()
+    {
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
+    }
+
 
     public void Move(Vector3 _velocity)
     {
